e41cd850359b31b2a28b4fca9da39f4e935ff67f
arfan <arfanawae084@gmail.com>
2026-07-05 16:59:52 +0700

    first commit

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return '<h1>Hello, World!</h1><p>Continuous Integration and Continuous Deployment is working.</p>'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+
