44cb63221974a94a22bc71dcd2b9d6cda813b6f6
arfan <arfanawae084@gmail.com>
2026-07-05 17:41:20 +0700

    เปลี่ยนหัวข้อ

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '<h1>Hello, World!</h1><p>Continuous Integration and Continuous Deployment is working.</p>'
+    return '<h1>Hello this my new website!</h1><p>Continuous Integration and Continuous Deployment is working.</p>'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
